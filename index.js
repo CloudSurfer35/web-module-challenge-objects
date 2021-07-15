5b577d6f7c370a4d80da38d0beb5697e5127614c
Matt Hunt <matthew-hunt@lambdastudents.com>
2021-07-15 12:33:50 -0700

    solved task 1

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
+ergergerg
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
